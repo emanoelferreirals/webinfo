3b60efa295674ebe6fc024cbede5b2764d10a340
emanoelferreirals <emanoelfls.dev@gmail.com>
2024-08-17 00:48:21 -0300

    initial commit

---
+++ blog/public/index.php
@@ -0,0 +1,10 @@
+<?php
+    echo "olá mundo!";
+?>
+<br>
+<a href="admin">Admin</a><br>
+<a href="conta">Cinta</a><br>
+<a href="newslater">Newslater</a><br>
+<a href="publi">Publi</a><br>
+<a href="search">Search</a><br>
+<a href="sobre">Sobre</a><br>
